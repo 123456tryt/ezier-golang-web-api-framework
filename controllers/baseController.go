977e8bd178244764ac61b98a9dee9bf558a92f1e
Eric <dejavuzhou@qq.com>
2017-11-09 21:11:55 +0800

    fix

--- controllers/baseController.go
+++ controllers/baseController.go
@@ -21,6 +21,8 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 }
 
 func jsonReturn(data interface{}, code uint8, msg string, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	p := map[string]interface{}{"code": code, "message": msg, "data": data}
 	json.NewEncoder(w).Encode(p)
 }
