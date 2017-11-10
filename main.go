24b263287662e942ff23dd10d7ac1839112cde7c
Eric <dejavuzhou@qq.com>
2017-11-10 19:50:33 +0800

    命令行中自定义端口

--- main.go
+++ main.go
@@ -3,21 +3,33 @@ package main
 import (
 	_ "ezier/models"
 	"ezier/routers"
+	"flag"
 	"fmt"
 
-	"flag"
 	"net/http"
 	"time"
 )
 
+func init() {
+}
+
 func main() {
-	addr := flag.String("addr", ":9999", "Port to listen on")
+	port := flag.String("port", ":1234", "help message for flagname")
+	flag.Parse()
+
+	//http://qefee.com/2014/02/02/go%E8%AF%AD%E8%A8%80%E7%9A%84flag%E5%8C%85%E7%AE%80%E5%8D%95%E4%BD%BF%E7%94%A8%E6%95%99%E7%A8%8B/
 	server := &http.Server{
 		Handler:      routers.HttpServeMux,
-		Addr:         *addr,
+		Addr:         *port,
 		WriteTimeout: 2 * time.Second,
 		ReadTimeout:  2 * time.Second,
 	}
-	server.ListenAndServe()
-	fmt.Println("server is one port 9999")
+	errrr := server.ListenAndServe()
+	if errrr != nil {
+		fmt.Println(errrr)
+
+	} else {
+		fmt.Println("server is one port", *port)
+
+	}
 }
