b1792a646385eb23618a7ac86671c1d104ff67a7
Eric <dejavuzhou@qq.com>
2017-11-10 20:26:05 +0800

    优化log

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	_ "ezier/models"
 	"ezier/routers"
 	"flag"
-	"fmt"
+	"log"
 
 	"net/http"
 	"time"
@@ -16,7 +16,6 @@ func init() {
 func main() {
 	port := flag.String("port", ":1234", "help message for flagname")
 	flag.Parse()
-
 	//http://qefee.com/2014/02/02/go%E8%AF%AD%E8%A8%80%E7%9A%84flag%E5%8C%85%E7%AE%80%E5%8D%95%E4%BD%BF%E7%94%A8%E6%95%99%E7%A8%8B/
 	server := &http.Server{
 		Handler:      routers.HttpServeMux,
@@ -24,12 +23,9 @@ func main() {
 		WriteTimeout: 2 * time.Second,
 		ReadTimeout:  2 * time.Second,
 	}
+	log.Println("[INFO] http服务将在http://localhost", *port, "启动......")
 	errrr := server.ListenAndServe()
 	if errrr != nil {
-		fmt.Println(errrr)
-
-	} else {
-		fmt.Println("server is one port", *port)
-
+		log.Fatalln("[ERROR] %s\n", errrr)
 	}
 }
